89431038f7b7c278e16618db6e0699185da05ecf
Grace <greyhz45@gmail.com>
2022-03-12 15:24:42 -0500

    03.12 addtlfixes

--- frontend/src/components/Test.js
+++ frontend/src/components/Test.js
@@ -2,9 +2,18 @@ import React, {Component} from 'react';
 
 function Test1 () {
         console.log("Test1 compo")
-        return (
 
-            <p>test</p>
+        const procDate = () => {
+            const tme = "11:30"
+            const d = new Date(0,0,0,tme);
+            console.log((d.getHours() * 3600) + (d.getMinutes() * 60));
+        }
+
+        return (
+            <>
+                {procDate()}
+                <p>test</p>
+            </>
         );
 
 
